15db4986b279406bd2791b90e31cd3b10b62eea6
ii.maschikevich <ii.maschikevich@csltd.com.ua>
2025-06-23 14:00:18 +0300

    fix lombok

--- src/main/java/net/west/springsecurityapp/model/Role.java
+++ src/main/java/net/west/springsecurityapp/model/Role.java
@@ -1,6 +1,9 @@
 package net.west.springsecurityapp.model;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -11,6 +14,8 @@ import java.util.Set;
  * @version 1.0
  */
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -24,33 +29,6 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
-
-    public Role() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
 
     @Override
     public String toString() {
--- src/main/java/net/west/springsecurityapp/model/User.java
+++ src/main/java/net/west/springsecurityapp/model/User.java
@@ -1,5 +1,8 @@
 package net.west.springsecurityapp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -10,6 +13,8 @@ import java.util.Set;
  * @version 1.0
  */
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
@@ -31,44 +36,4 @@ public class User {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
